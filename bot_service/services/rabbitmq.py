b8735b28b5067117666484632a112c9852688743
GithubDenis5 <gromacihindenis@gmail.com>
2025-03-02 00:37:02 +0300

    fix: rabbitmq immortal queues

--- bot_service/services/rabbitmq.py
+++ bot_service/services/rabbitmq.py
@@ -6,28 +6,20 @@ import uuid
 class RabbitMQ:
     def __init__(self, rabbitmq_url: str):
         self.rabbitmq_url = rabbitmq_url
-        self.connection = None
-        self.channel = None
         self.responses = {}
-
-    async def connect(self):
-        """Устанавливает соединение с RabbitMQ (если не установлено)"""
-        if not self.connection:
-            self.connection = await aio_pika.connect_robust(self.rabbitmq_url)
-            self.channel = await self.connection.channel()
 
     async def send_message(self, queue_name: str, message: str) -> str:
         """Отправляет сообщение и ожидает ответ с тем же correlation_id"""
-        await self.connect()
+        connection = await aio_pika.connect_robust(self.rabbitmq_url)
+        channel = await connection.channel()
 
-        correlation_id = str(uuid.uuid4())  # Генерируем уникальный ID
-        callback_queue = await self.channel.declare_queue(exclusive=True)  # Временная очередь
+        correlation_id = str(uuid.uuid4())
+        callback_queue = await channel.declare_queue(exclusive=True)
 
         future = asyncio.Future()
-        self.responses[correlation_id] = future  # Сохраняем ожидание ответа
+        self.responses[correlation_id] = future
 
-        # Отправляем сообщение с `correlation_id` и `reply_to`
-        await self.channel.default_exchange.publish(
+        await channel.default_exchange.publish(
             aio_pika.Message(
                 body=message.encode(),
                 correlation_id=correlation_id,
@@ -36,17 +28,20 @@ class RabbitMQ:
             routing_key=queue_name,
         )
 
-        # Подписываемся на очередь ответов
         await callback_queue.consume(self._on_response)
 
-        return await future  # Ожидаем ответ
+        try:
+            return await future
+        finally:
+            await connection.close()
 
     async def _on_response(self, message: aio_pika.IncomingMessage):
-        """Обрабатывает ответ и передает его в соответствующий Future"""
-        correlation_id = message.correlation_id
-        if correlation_id in self.responses:
-            future = self.responses.pop(correlation_id)
-            future.set_result(message.body.decode())
+        """Обрабатывает ответ и подтверждает получение"""
+        async with message.process():
+            correlation_id = message.correlation_id
+            if correlation_id in self.responses:
+                future = self.responses.pop(correlation_id)
+                future.set_result(message.body.decode())
 
 
 # Глобальный клиент RabbitMQ
