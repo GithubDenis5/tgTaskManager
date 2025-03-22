9cd5a54d6e5c62f53746e54d648618ce21ae4cee
GithubDenis5 <gromacihindenis@gmail.com>
2025-03-22 20:12:00 +0300

    fix: fix return button

--- bot_service/handler.py
+++ bot_service/handler.py
@@ -1,16 +1,11 @@
 from bot_service.utils import keyboards
 from bot_service.logger import setup_logger
 from aiogram import F, Router
-from aiogram.filters import StateFilter
-from aiogram.filters import Command, CommandStart
+from aiogram.filters import CommandStart
 from aiogram.fsm.context import FSMContext
 from aiogram.types import (
     CallbackQuery,
-    FSInputFile,
-    InputMediaDocument,
     Message,
-    ReplyKeyboardRemove,
-    TelegramObject,
 )
 from bot_service.config import messages, labels, states
 
@@ -55,6 +50,19 @@ async def show_task_list_message(message: Message, state: FSMContext):
     text = await formaters.format_tasks_list(tasks)
 
     await message.answer(text=text, reply_markup=keyboards.under_tasks_list_keyboard)
+
+
+@user.callback_query(F.data == "show_tasks_list_message")
+async def show_task_list_message_callback(callback: CallbackQuery, state: FSMContext):
+    logger.debug(f"user {callback.from_user.id} ask tasks list")
+    await state.clear()
+    await callback.answer()
+
+    tasks = await task_service.get_tasks(callback.from_user.id)
+
+    text = await formaters.format_tasks_list(tasks)
+
+    await callback.message.edit_text(text=text, reply_markup=keyboards.under_tasks_list_keyboard)
 
 
 @user.message(F.text == labels.ADD_TASK)
